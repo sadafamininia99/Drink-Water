d714f69ff927ed39a43fcfe475fb64c3c5df6f26
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2021-08-08 15:14:02 +0430

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
-
+updateBigCup()
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 })
-
-
 
 function highlightCups(idx) {
     if (idx===7 && smallCups[idx].classList.contains("full")) idx--;
@@ -25,5 +23,28 @@ function highlightCups(idx) {
         }
     })
 
-   
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    //for cheack fullcups :
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
 }
